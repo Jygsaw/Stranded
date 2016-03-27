98c8f49d12c760da65d675229ca20f2883cd45fa
Justin Cheung <jcheung@crunchyroll.com>
2016-03-27 01:59:19 -0700

    [NEW] Create itemInfo directives

---
+++ app/directives/itemInfo/itemInfo.js
@@ -0,0 +1,87 @@
+(function () {
+  'use strict';
+
+  // initialize module
+  var app = angular.module('strandedApp.itemInfoDirective', [
+  ]);
+
+  // declare directive
+  app.directive('itemInfo', [
+    '$compile',
+    function ($compile) {
+      return {
+        restrict: 'A',
+        scope: {
+          item: '=',
+        },
+        link: function (scope, elem) {
+          // dynamically choose display directive based on item type
+          var dynaTag = '<div ' + scope.item.type + '-info item="item"></div>';
+          elem.append($compile(dynaTag)(scope));
+        },
+      };
+    }
+  ]);
+
+  // declare display directives
+  app.directive('armorInfo', [
+    function () {
+      return {
+        scope: {
+          item: '=',
+        },
+        templateUrl: 'directives/itemInfo/templates/armorInfo.html',
+      };
+    }
+  ]);
+  app.directive('engineInfo', [
+    function () {
+      return {
+        scope: {
+          item: '=',
+        },
+        templateUrl: 'directives/itemInfo/templates/engineInfo.html',
+      };
+    }
+  ]);
+  app.directive('frameInfo', [
+    function () {
+      return {
+        scope: {
+          item: '=',
+        },
+        templateUrl: 'directives/itemInfo/templates/frameInfo.html',
+      };
+    }
+  ]);
+  app.directive('moduleInfo', [
+    function () {
+      return {
+        scope: {
+          item: '=',
+        },
+        templateUrl: 'directives/itemInfo/templates/moduleInfo.html',
+      };
+    }
+  ]);
+  app.directive('shieldInfo', [
+    function () {
+      return {
+        scope: {
+          item: '=',
+        },
+        templateUrl: 'directives/itemInfo/templates/shieldInfo.html',
+      };
+    }
+  ]);
+  app.directive('weaponInfo', [
+    function () {
+      return {
+        scope: {
+          item: '=',
+        },
+        templateUrl: 'directives/itemInfo/templates/weaponInfo.html',
+      };
+    }
+  ]);
+}());
