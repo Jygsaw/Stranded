b1f56ff75fab1e0fa3bbca5118d2beeaba6f136e
Justin Cheung <jcheung@crunchyroll.com>
2016-03-29 02:05:38 -0700

    [NEW] Implement design-parts ui

--- app/views/design/design.js
+++ app/views/design/design.js
@@ -4,6 +4,9 @@
   // initialize module
   var app = angular.module('strandedApp.design', [
     'ui.router',
+    'strandedApp.filters',
+    'strandedApp.inventoryService',
+    'strandedApp.itemInfoDirective',
   ]);
 
   // configure route
@@ -21,8 +24,17 @@
   // declare controller
   app.controller('DesignController', [
     '$scope',
-    function ($scope) {
+    'InventoryService',
+    function ($scope, InventoryService) {
       // initialize variables
+      $scope.parts = {
+        armors: InventoryService.getArmors(),
+        engines: InventoryService.getEngines(),
+        frames: InventoryService.getFrames(),
+        modules: InventoryService.getModules(),
+        shields: InventoryService.getShields(),
+        weapons: InventoryService.getWeapons(),
+      };
 
       // declare methods
     }
