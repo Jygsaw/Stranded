eb331486fabaf150edac53b03898ffc0d13dd5e8
Justin Cheung <jcheung@crunchyroll.com>
2016-03-30 00:50:24 -0700

    [NEW] Create node server

---
+++ server.js
@@ -0,0 +1,19 @@
+// initialize variables
+var express = require('express');
+var app = express();
+
+// specify port
+app.set('port', (process.env.PORT || 5000));
+
+// declare static assets
+app.use(express.static(__dirname + '/app'));
+
+// declare routes
+app.get('/', function(request, response) {
+  response.render('app/index');
+});
+
+// start web server
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
