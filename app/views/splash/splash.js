dec739cf78c5de345ee99c1712d4e032ea2a3483
Justin Cheung <jcheung@crunchyroll.com>
2016-04-02 01:43:41 -0700

    [CHG] Inject splash.css

--- app/views/splash/splash.js
+++ app/views/splash/splash.js
@@ -4,6 +4,7 @@
   // initialize module
   var app = angular.module('strandedApp.splash', [
     'ui.router',
+    'strandedApp.injectCssService',
   ]);
 
   // configure state
@@ -13,6 +14,14 @@
       $stateProvider.state('splash', {
         url: '/splash',
         templateUrl: 'views/splash/splash.html',
+        resolve: {
+          css: [
+            'InjectCssService',
+            function (InjectCssService) {
+              InjectCssService.inject('views/splash/splash.css');
+            }
+          ],
+        },
       });
     }
   ]);
